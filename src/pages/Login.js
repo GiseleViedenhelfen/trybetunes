99b8808fe3d39c710dbbe724889da84795e4ab74
Gisele <gisele.viedenhelfen@gmail.com>
2022-04-18 18:20:59 -0300

    requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,61 @@
 import { Component } from 'react/cjs/react.production.min';
+import { createUser } from '../services/userAPI';
 
 const React = require('react');
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isButtonDisable: true,
+      inputValue: '',
+    };
+    this.handleInputValue = this.handleInputValue.bind(this);
+  }
+
+  handleInputValue(e) {
+    const { value } = e.target;
+    this.setState({ inputValue: value }, () => {
+      const valueLength = e.target.value.length;
+      const minInputLength = 3;
+      if (valueLength >= minInputLength) {
+        this.setState({ isButtonDisable: false });
+      } else {
+        this.setState({ isButtonDisable: true });
+      }
+    });
+  }
+
+  handleClick() {
+    <p>carregando...</p>;
+    createUser();
+  }
+
   render() {
+    const { isButtonDisable, inputValue } = this.state;
+    console.log(inputValue);
     return (
       <div data-testid="page-login">
-        <h1>Pagina de login</h1>
+        <form>
+          <label htmlFor="input-name">
+            Nome:
+            <input
+              type="text"
+              id="input-name"
+              placeholder="nome"
+              data-testid="login-name-input"
+              onChange={ this.handleInputValue }
+            />
+          </label>
+          <button
+            type="submit"
+            data-testid="login-submit-button"
+            disabled={ isButtonDisable }
+            onClick={ createUser }
+          >
+            Entrar
+          </button>
+        </form>
       </div>
     );
   }
